e9628c3f02e98f0df819689a288c24f1cb4927c7
f-patel <36059613+f-patel@users.noreply.github.com>
2019-08-20 12:08:40 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MoviesComponent from "./components/MoviesComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <MoviesComponent />
+    </div>
+  );
+}
+
+export default App;
